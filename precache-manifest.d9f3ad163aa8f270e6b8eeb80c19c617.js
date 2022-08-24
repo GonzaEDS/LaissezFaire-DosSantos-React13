b4cb2f04068d289bb3499488b0cbf05e2dc5283a
Gonzalo Dos Santos <die.zaubernacht@gmail.com>
2022-08-24 05:43:52 -0300

    Updates

---
+++ precache-manifest.d9f3ad163aa8f270e6b8eeb80c19c617.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cec1335c3a3ab743021e",
+    "url": "/LaissezFaire-DosSantos-React13/static/css/main.61119dee.chunk.css"
+  },
+  {
+    "revision": "cec1335c3a3ab743021e",
+    "url": "/LaissezFaire-DosSantos-React13/static/js/main.51438105.chunk.js"
+  },
+  {
+    "revision": "308f4b0e1ab2b598ad9a",
+    "url": "/LaissezFaire-DosSantos-React13/static/js/runtime~main.08ff3c0e.js"
+  },
+  {
+    "revision": "b5e3d8d4e211cf79f645",
+    "url": "/LaissezFaire-DosSantos-React13/static/css/2.9f54ecab.chunk.css"
+  },
+  {
+    "revision": "b5e3d8d4e211cf79f645",
+    "url": "/LaissezFaire-DosSantos-React13/static/js/2.48b829f3.chunk.js"
+  },
+  {
+    "revision": "dfef2813c51b7c1e772e",
+    "url": "/LaissezFaire-DosSantos-React13/static/js/3.c015642e.chunk.js"
+  },
+  {
+    "revision": "b57a58905370284f1499d1a35f5d2c96",
+    "url": "/LaissezFaire-DosSantos-React13/static/media/bootstrap-icons.b57a5890.woff2"
+  },
+  {
+    "revision": "a6a4862c8f0a585c6c1f6d5aa07b761c",
+    "url": "/LaissezFaire-DosSantos-React13/static/media/bootstrap-icons.a6a4862c.woff"
+  },
+  {
+    "revision": "ad9aaeb6c0f21818d377aa69c3763c0a",
+    "url": "/LaissezFaire-DosSantos-React13/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/LaissezFaire-DosSantos-React13/precache-manifest.d9f3ad163aa8f270e6b8eeb80c19c617.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/LaissezFaire-DosSantos-React13/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
